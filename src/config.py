dc52ef627c5d9d08bcec16607a4c160e01291e1a
ajoshi24 <ajoshi24@ncsu.edu>
2024-07-10 18:30:41 -0400

    Add comments to config.py

--- src/config.py
+++ src/config.py
@@ -1,6 +1,10 @@
 import os
 
+# Get the absolute path of the directory where the current file is located
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+# Define the database URI for SQLAlchemy, using SQLite database located in the same directory
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'weather_data.db')
+
+# Disable modification tracking to save resources
 SQLALCHEMY_TRACK_MODIFICATIONS = False
